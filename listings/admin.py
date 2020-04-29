09a516d5efa3caf86c6a4e3644cfeb6c13750307
Greg <gsc229@gmail.com>
2020-04-28 22:17:15 -0400

    pagination working

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin): # this allows us to change all the fields displayed for each 
+                                      # listing in the listings section of the admin app
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)

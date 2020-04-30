2bf43bd58415385138adc40eee76f2cdb500deb1
Greg <gsc229@gmail.com>
2020-04-29 23:11:51 -0400

    seller of the month. and realtors data dynamically rendered

--- pages/views.py
+++ pages/views.py
@@ -14,8 +14,13 @@ def index(request):
 
 
 def about(request):
-    realtors = Realtor.objects()[:3]
+    realtors = Realtor.objects.order_by('-hire_date')
+    
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
     context = {
-        'realtors': realtors
+        'realtors': realtors, 
+        'mvp_realtors': mvp_realtors
     }
+
     return render(request, 'pages/about.html', context)

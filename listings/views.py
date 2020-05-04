5f7a2e150e082973ede02dcc08c343ae585ceac0
Greg <gsc229@gmail.com>
2020-05-03 22:51:27 -0400

    search

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
-
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 def index(request):
@@ -31,4 +31,15 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+ 
+    queryset_list = Listing.objects.order_by('-list_date')
+
+
+    context = {
+        'listings': queryset_list,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+
+    return render(request, 'listings/search.html', context)

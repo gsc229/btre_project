6034509ec1b0751daeb8d76f158b8aee0b8c720c
Greg <gsc229@gmail.com>
2020-05-04 21:40:25 -0400

    hidden inputs for user_id in the lising inquiry form

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
+# this allows us to change all the fields displayed for each 
+# contact in the contact  section of the admin app (our admin area we actually sign into through the browser)
+class ContactAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+  list_display_links = ('id', 'name')
+  search_fields = ('name', 'email', 'listing')
+  list_per_page = 25
+admin.site.register(Contact, ContactAdmin)
 
-# Register your models here.
--- listings/admin.py
+++ listings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin): # this allows us to change all the fields displayed for each 
-                                      # listing in the listings section of the admin app
+                                      # listing in the listings section of the admin app (our admin area we actually sign into through the browser)
     list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
     list_display_links = ('id', 'title')
     list_filter = ('realtor',)

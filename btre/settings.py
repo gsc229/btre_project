6bee4bebecbed909a3f84e1b0bb15ec36a43f6f3
Greg <gsc229@gmail.com>
2020-05-09 19:40:33 -0400

    installed white noise to serve static files in production. (Django does not support serving static files in production

--- btre/settings.py
+++ btre/settings.py
@@ -14,6 +14,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')

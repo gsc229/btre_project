d42146fec04843419fe02b05783fc6efb7ad4ef0
Greg <gsc229@gmail.com>
2020-05-10 13:27:12 -0400

    hooked up email service. made added environment vars to heroku. using python-dotenv

--- btre/settings.py
+++ btre/settings.py
@@ -145,5 +145,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',    
 }
 
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER= os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD= os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL=os.getenv('EMAIL_USE_SSL')
+
+
+
 
 django_heroku.settings(locals())
